02ee4155503c84834d0af2003040828b4d851d82
Lê Minh Toàn <109668965+Hissatsu265@users.noreply.github.com>
2025-02-18 04:38:05 -0800

    Create webkotlin.kt

---
+++ webkotlin.kt
@@ -0,0 +1,31 @@
+import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        install(DefaultHeaders)
+        install(ContentNegotiation) { jackson {} }
+        install(StatusPages) {
+            exception<Throwable> { cause ->
+                call.respond(HttpStatusCode.InternalServerError, mapOf("error" to cause.localizedMessage))
+            }
+        }
+        routing {
+            get("/") {
+                call.respond(mapOf("message" to "Welcome to Kotlin Web API!"))
+            }
+            get("/hello") {
+                call.respond(mapOf("greeting" to "Hello, World!"))
+            }
+        }
+    }.start(wait = true)
+}
