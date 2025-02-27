0299450656cbfe3e1d5f21009922c3c958010dce
Lê Minh Toàn <109668965+Hissatsu265@users.noreply.github.com>
2025-02-27 20:24:33 +0700

    Update cv.py

--- cv.py
+++ cv.py
@@ -8,8 +8,6 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import torch.nn as nn
-import torch.nn as nn
-
 # Load Pretrained ResNet18
 class CNN_ResNet(nn.Module):
     def __init__(self, num_classes=10):
