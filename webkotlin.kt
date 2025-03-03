099a672fb2a721c11f3b2bdb12f5866b4a4944f1
Lê Minh Toàn <109668965+Hissatsu265@users.noreply.github.com>
2025-03-03 19:12:08 +0700

    Update webkotlin.kt

--- webkotlin.kt
+++ webkotlin.kt
@@ -1,4 +1,15 @@
 
+import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 fun main() {
     embeddedServer(Netty, port = 8080) {
         install(DefaultHeaders)
